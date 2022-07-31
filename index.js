83d93372a16972d94dc900b7d6b49d28c4983d4f
Amir Malkani <91412094+amirmalkani@users.noreply.github.com>
2022-07-31 11:46:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,27 +5,47 @@ let pass = document.getElementById("pass");
 let mainPage = document.querySelector('.mainPage');
 let mainPage2 = document.querySelector('.mainPage2');
 let btn = document.querySelector(".btn");
-unlock.addEventListener('click',function(){
-    navigator.getBattery().then(function(battery) {
-        if(pass.value == battery.level*100){
+let nameHolder = document.querySelector('.nameHolder');
+let loginBtn = document.querySelector('#loginBtn');
+let name = document.getElementById('name');
+let nameInput = document.getElementById("nameInput");
+
+loginBtn.addEventListener('click', function () {
+    nameHolder.classList.add("hide");
+    name.innerText = `${nameInput.value.toUpperCase()} Please Enter Your Password`
+    container.classList.remove("hide");
+    nameInput.value="";
+});
+unlock.addEventListener('click', function () {
+    navigator.getBattery().then(function (battery) {
+        if (pass.value == battery.level * 100) {
             mainPage.classList.remove('hide');
             container.classList.add('hide');
             btn.classList.remove("hide")
             btn.innerText = "Lock"
-            pass.value="";
+            pass.value = "";
         }
-        else{
+        else {
             mainPage2.classList.remove('hide')
             container.classList.add('hide');
             btn.classList.remove("hide")
             btn.innerText = "Try Again"
-            pass.value="";
+            pass.value = "";
         }
-    })
+    })  
 });
-btn.addEventListener('click',function(){
-    container.classList.remove('hide');
-    mainPage2.classList.add('hide')
-    btn.classList.add('hide')
-    mainPage.classList.add('hide');
+btn.addEventListener('click', function () {
+    if(btn.innerText == 'Lock'){
+        container.classList.add('hide');
+        mainPage2.classList.add('hide')
+        btn.classList.add('hide')
+        mainPage.classList.add('hide');
+        nameHolder.classList.remove('hide');
+    }
+    else{
+        container.classList.remove('hide');
+        mainPage2.classList.add('hide')
+        btn.classList.add('hide')
+        mainPage.classList.add('hide');
+    }
 })
